efc1bb4d0fff7085fc260fa8142199d1d16537c1
Jurdzi <161888304+Jurdzi@users.noreply.github.com>
2024-05-14 20:49:36 +0200

    Add files via upload

--- test_ES.R
+++ test_ES.R
@@ -221,43 +221,51 @@ test_ES_AST <- function(data = NULL, percentile = NULL) {
   
   ES_X <- rep(0,length(data[1,]))
   
+  x <- data[1,]
+  mu <- data[2,]
+  sigma <- exp(data[3,])
+  alfa <- exp(data[4,])/(1+exp(data[4,]))
+  nu_1 <- exp(data[5,])
+  nu_2 <- exp(data[6,])
+  
   if(percentile <= 0.5){
     
-    for (i in 1:length(data[1,])) {
-      if(percentile <= exp(data[4,i])){
-        ES_X[i] <- data[2,i] - (2*exp(data[4,i]))^2 * K_func(exp(data[5,i])) * exp(data[3,i]) / (percentile*(exp(data[5,i])-1)) * T_nu(x = percentile/(2*exp(data[4,i])), nu = exp(data[5,i]))
+    for (i in 1:length(x)) {
+      if(percentile <= alfa[i]){
+        ES_X[i] <- mu[i] - 4*alfa[i]^2 * K_func(nu_1[i]) * sigma[i] / (percentile*(nu_1[i]-1)) * T_nu(x = percentile/(2*alfa[i]), nu = nu_1[i])
+        #ES_X[i] <- mu[i] - (2*alfa[i]*K_func(nu_1[i]))^2*sigma[i]*nu_1[i] / (alfa[i]*(nu_1[i]-1)) * (1+qt(percentile/(2*alfa[i]), nu_1[i])/nu_1[i])^((1-nu_1[i])/2)
       }else{
-        ES_X[i] <- data[2,i] - ( 2*(1-exp(data[4,i])) )^2 * K_func(exp(data[6,i])) * exp(data[3,i]) / (percentile*(exp(data[6,i])-1)) * T_nu(x = (percentile+1-2*exp(data[4,i])) / (2*(1-exp(data[4,i]))), nu = exp(data[6,i]) ) - h_AST(data = data[,i] ) / percentile
+        ES_X[i] <- mu[i] -  4*(1-alfa[i])^2 * K_func(nu_2[i]) * sigma[i] / (percentile*(nu_2[i]-1)) * T_nu(x = (percentile+1-2*alfa[i]) / (2*(1-alfa[i])), nu = nu_2[i] ) - h_AST(data = data[,i] ) / percentile
       }
     }
     
     I <- ifelse(ES_X>data[1,], 1, 0)
     k <- sum(I)
-    P <- pAST(ES_X, mu = data[2,], sigma = exp(data[3,]), alfa = exp(data[4,]), nu_1 = exp(data[5,]), nu_2 = exp(data[6,]))
-    mu <- sum(P)
-    sigma <- sqrt(sum(P*(1-P)))
+    P <- pAST(ES_X, mu = mu, sigma = sigma, alfa = alfa, nu_1 = nu_1, nu_2 = nu_2)
+    mu_1 <- sum(P)
+    sigma_1 <- sqrt(sum(P*(1-P)))
     
   }else{
     
-    for (i in 1:length(data[1,])) {
-      if(percentile > exp(data[4,i])){
-        ES_X[i] <- data[2,i] + (2*exp(data[4,i]))^2 * K_func(exp(data[6,i])) * exp(data[3,i]) / ((1-percentile)*(exp(data[6,i])-1)) * T_nu(x = (percentile+1-2*exp(data[4,i])) / (2*(1-exp(data[4,i]))), nu = exp(data[6,i]))
+    for (i in 1:length(x)) {
+      if(percentile > alfa[i]){
+        ES_X[i] <- mu[i] + 4*alfa[i]^2 * K_func(nu_1[i]) * sigma[i] / ((1-percentile)*(nu_2[i]-1)) * T_nu(x = (percentile+1-2*alfa[i]) / (2*(1-alfa[i])), nu = nu_2[i])
       }else{
-        ES_X[i] <- data[2,i] + ( 2*(1-exp(data[4,i])) )^2 * K_func(exp(data[5,i])) * exp(data[3,i]) / ((1-percentile)*(exp(data[5,i])-1)) * T_nu(x = percentile / (2*exp(data[4,])), nu = exp(data[5,i]) ) + h_AST(data = data[,i]) / (1-percentile)
+        ES_X[i] <- mu[i] + 4*(1-alfa[i])^2 * K_func(nu_1[i]) * sigma[i] / ((1-percentile)*(nu_1[i]-1)) * T_nu(x = percentile / (2*alfa[i]), nu = nu_1[i] ) + h_AST(data = data[,i]) / (1-percentile)
       }
     }
     
     I <- ifelse(ES_X<data[1,], 1, 0)
     k <- sum(I)
-    P <- 1 - pAST(ES_X, mu = data[2,], sigma = exp(data[3,]), alfa = exp(data[4,]), nu_1 = exp(data[5,]), nu_2 = exp(data[6,]))
-    mu <- sum(P)
-    sigma <- sqrt(sum(P*(1-P)))
+    P <- 1- pAST(ES_X, mu = mu, sigma = sigma, alfa = alfa, nu_1 = nu_1, nu_2 = nu_2)
+    mu_1 <- sum(P)
+    sigma_1 <- sqrt(sum(P*(1-P)))
   }
   
-  if(k <= mu - 0.5){
-    LR_uc <- 2*pnorm((k + 0.5 - mu)/sigma, 0, 1, lower.tail = TRUE)
+  if(k <= mu_1 - 0.5){
+    LR_uc <- 2*pnorm((k + 0.5 - mu_1)/sigma_1, 0, 1, lower.tail = TRUE)
   }else{
-    LR_uc <- 2*pnorm((k + 0.5 - mu)/sigma, 0, 1, lower.tail = FALSE)
+    LR_uc <- 2*pnorm((k + 0.5 - mu_1)/sigma_1, 0, 1, lower.tail = FALSE)
   }
   
   LR_ind <- test_LR_ind(I)
@@ -270,48 +278,52 @@ test_ES_SST <- function(data = NULL, percentile = NULL) {
   
   ES_X <- rep(0,length(data[1,]))
   
+  x <- data[1,]
+  mu <- data[2,]
+  sigma <- exp(data[3,])
+  alfa <- exp(data[4,])/(1+exp(data[4,]))
+  nu <- exp(data[5,])
+  
   if(percentile <= 0.5){
     
-    for (i in 1:length(data[1,])) {
-      if(percentile <= exp(data[4,i])){
-        ES_X[i] <- data[2,i] - (2*exp(data[4,i]))^2 * K_func(exp(data[5,i])) * exp(data[3,i]) / (percentile*(exp(data[5,i])-1)) * T_nu(x = percentile/(2*exp(data[4,i])), nu = exp(data[5,i]))
+    for (i in 1:length(x)) {
+      if(percentile <= alfa[i]){
+        ES_X[i] <- mu[i] - (2*alfa[i])^2 * K_func(nu[i]) * sigma[i] / (percentile*(nu[i]-1)) * T_nu(x = percentile/(2*alfa[i]), nu = nu[i])
       }else{
-        ES_X[i] <- data[2,i] - ( 2*(1-exp(data[4,i])) )^2 * K_func(exp(data[5,i])) * exp(data[3,i]) / (percentile*(exp(data[5,i])-1)) * T_nu(x = (percentile+1-2*exp(data[4,i])) / (2*(1-exp(data[4,i]))), nu = exp(data[5,i]) ) - h_SST(data = data[,i] ) / percentile
+        ES_X[i] <- mu[i] - ( 2*(1-alfa[i]) )^2 * K_func(nu[i]) * sigma[i] / (percentile*(nu[i]-1)) * T_nu(x = (percentile+1-2*alfa[i]) / (2*(1-alfa[i])), nu = nu[i] ) - h_SST(data = data[,i] ) / percentile
       }
     }
     
-    I <- ifelse(ES_X>data[1,], 1, 0)
+    I <- ifelse(ES_X>x, 1, 0)
     k <- sum(I)
-    P <- pSST(ES_X, mu = data[2,], sigma = exp(data[3,]), alfa = exp(data[4,]), nu = exp(data[5,]))
-    mu <- sum(P)
-    sigma <- sqrt(sum(P*(1-P)))
+    P <- pSST(ES_X, mu = mu, sigma = sigma, alfa = alfa, nu = nu)
+    mu_1 <- sum(P)
+    sigma_1 <- sqrt(sum(P*(1-P)))
     
   }else{
     
-    for (i in 1:length(data[1,])) {
-      if(percentile > exp(data[4,i])){
-        ES_X[i] <- data[2,i] + (2*exp(data[4,i]))^2 * K_func(exp(data[5,i])) * exp(data[3,i]) / ((1-percentile)*(exp(data[5,i])-1)) * T_nu(x = (percentile+1-2*exp(data[4,i])) / (2*(1-exp(data[4,i]))), nu = exp(data[5,i]))
+    for (i in 1:length(x)) {
+      if(percentile > alfa[i]){
+        ES_X[i] <- mu[i] + (2*alfa[i])^2 * K_func(nu[i]) * sigma[i] / ((1-percentile)*(nu[i]-1)) * T_nu(x = (percentile+1-2*alfa[i]) / (2*(1-alfa[i])), nu = nu[i])
       }else{
-        ES_X[i] <- data[2,i] + ( 2*(1-exp(data[4,i])) )^2 * K_func(exp(data[5,i])) * exp(data[3,i]) / ((1-percentile)*(exp(data[5,i])-1)) * T_nu(x = percentile / 2*exp(data[4,i]), nu = exp(data[5,i]) ) + h_SST(data = data[,i]) / (1-percentile)
+        ES_X[i] <- mu[i] + ( 2*(1-alfa[i]) )^2 * K_func(nu[i]) * sigma[i] / ((1-percentile)*(nu[i]-1)) * T_nu(x = percentile / (2*alfa[i]), nu = nu[i] ) + h_SST(data = data[,i]) / (1-percentile)
       }
     }
     
     I <- ifelse(ES_X<data[1,], 1, 0)
     k <- sum(I)
-    P <- 1 - pSST(ES_X, mu = data[2,], sigma = exp(data[3,]), alfa = exp(data[4,]), nu = exp(data[5,]))
-    mu <- sum(P)
-    sigma <- sqrt(sum(P*(1-P)))
+    P <- 1- pSST(ES_X, mu = mu, sigma = sigma, alfa = alfa, nu = nu)
+    mu_1 <- sum(P)
+    sigma_1 <- sqrt(sum(P*(1-P)))
   }
   
-  if(k <= mu - 0.5){
-    LR_uc <- 2*pnorm((k + 0.5 - mu)/sigma, 0, 1, lower.tail = TRUE)
+  if(k <= mu_1 - 0.5){
+    LR_uc <- 2*pnorm((k + 0.5 - mu_1)/sigma_1, 0, 1, lower.tail = TRUE)
   }else{
-    LR_uc <- 2*pnorm((k + 0.5 - mu)/sigma, 0, 1, lower.tail = FALSE)
+    LR_uc <- 2*pnorm((k + 0.5 - mu_1)/sigma_1, 0, 1, lower.tail = FALSE)
   }
   
   LR_ind <- test_LR_ind(I)
   
   return(list(LR_uc = LR_uc, LR_ind = LR_ind, k = k))
 }
-
-
